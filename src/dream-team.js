a0b1a1a83c711a05d11281799b9696cd9b9bacc4
CerBeer <ocelopp@gmail.com>
2023-12-19 22:07:16 +0300

    feat: implements Dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,11 +13,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  let fch = members.reduce((res, elem) => {
+    if (typeof elem === 'string') {
+      res.push(elem.trim()[0].toUpperCase() || '');
+    }
+    return res;
+  }, []);
+
+  result = fch.sort().join('');
+  
+  return result.length > 0 ? result : false;
 }
 
 module.exports = {
   createDreamTeam
 };
+
+return;
+
+function assert_equal(answer, true_answer) {
+  console.log(`is ${answer === true_answer} ${JSON.stringify(answer)} = ${JSON.stringify(true_answer)}`);
+}
+
+assert_equal(createDreamTeam(['Keith', 'Donna', 'Lawrence', 'Sophia', 'Jack', 'Kyle', 'Andrew', 'Natalie', 'Deborah', 'Noah']), 'ADDJKKLNNS');
+assert_equal(createDreamTeam(['Sophia', 'Margaret', 'Maria', 'Wayne', 'Julie', 'Samantha', 'Deborah', 'Diana', 'Bruce', 'Jacob']), 'BDDJJMMSSW');
+assert_equal(createDreamTeam(['Kevin', 'Timothy', 'Jeffrey', 'Albert', 'Laura', 'Karen', 'Steven', 'Diana', 'Sandra', 'Emma']), 'ADEJKKLSST');
+
+assert_equal(createDreamTeam(3), false);
+assert_equal(createDreamTeam(3.312312), false);
+assert_equal(createDreamTeam(false), false);
+assert_equal(createDreamTeam(null), false);
+assert_equal(createDreamTeam(undefined), false);
+assert_equal(createDreamTeam({ 'foo': 'bar' }), false);
