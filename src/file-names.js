4f841fc79d8f2d024ecda527115dcc82ba41e578
CerBeer <ocelopp@gmail.com>
2023-12-20 00:30:09 +0300

    feat: File names

--- src/file-names.js
+++ src/file-names.js
@@ -15,11 +15,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  
+  const result = [];
+  const counter = {};
+  for (let i = 0; i < names.length; i += 1) {
+  
+    if (typeof(counter[names[i]]) === 'undefined') {
+      counter[names[i]] = 1;
+      result.push(names[i]);
+    } else {
+      const newname = `${names[i]}(${counter[names[i]]})`;
+      counter[names[i]] += 1;
+      counter[newname] = 1;
+      result.push(newname);
+    }
+  };
+
+  return result;
+
 }
 
 module.exports = {
   renameFiles
 };
+
+return;
+
+function assert_equal(answer, true_answer) {
+  answer_str = JSON.stringify(answer);
+  true_answer_str = JSON.stringify(true_answer);
+  reason = `${answer_str === true_answer_str}  `.slice(0, 6);
+  console.log(`is ${reason} : ${answer_str} = ${true_answer_str}`);
+}
+
+assert_equal(renameFiles(['doc', 'doc', 'image', 'doc(1)', 'doc']), ['doc', 'doc(1)', 'image', 'doc(1)(1)', 'doc(2)']);
+assert_equal(renameFiles(['a', 'b', 'cd', 'b ', 'a(3)']), ['a', 'b', 'cd', 'b ', 'a(3)']);
+assert_equal(renameFiles([]), []);
