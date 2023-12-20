ee5966c5098843b3ae8aafab33dd7c77e94622e6
CerBeer <ocelopp@gmail.com>
2023-12-21 01:26:00 +0300

    feat: implements What season

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,80 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(type = true) {
+    this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ 
+  encrypt(message, key) {
+		return this.encrypt_decrypt(message, key);
   }
+
+  decrypt(message, key) {
+		return this.encrypt_decrypt(message, key, false);
+  }
+
+  encrypt_decrypt(message, key, encrypt = true) {
+    
+    if (typeof message !== "string") throw new Error("Incorrect arguments!");
+    if (typeof key !== "string") throw new Error("Incorrect arguments!");
+    if (message.length * key.length === 0) throw new Error("Incorrect arguments!");
+
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const a = alphabet.split('');
+    const aS = alphabet[0];
+    const aE = alphabet.at(-1);
+		const m = message.toUpperCase().split('');
+    const repeatK = Math.ceil(message.length / key.length);
+		let k = key.toUpperCase().split('');
+    for (let i = 1; i < repeatK; i += 1) {
+      k = k.concat(k);
+    };
+
+    let result = [];
+		let iC = 0;
+    m.forEach((v, i) => {
+			if (v < aS || v > aE) {
+        result.push(message[i]);
+        iC += 1;
+      }
+      else {
+        let ind = 0;
+        if (encrypt) ind = (a.indexOf(v) + a.indexOf(k[i - iC])) % a.length;
+        else ind = (a.indexOf(v) - a.indexOf(k[i - iC]) + a.length) % a.length;
+        result.push(a[ind]);
+      };
+		});
+    if (!this.type) result.reverse();
+		return result.join('');
+  }
+
 }
 
 module.exports = {
   VigenereCipheringMachine
 };
+
+return;
+
+function assert_equal(answer, true_answer) {
+  answer_str = JSON.stringify(answer);
+  true_answer_str = JSON.stringify(true_answer);
+  reason = `${answer_str === true_answer_str}  `.slice(0, 6);
+  console.log(`is ${reason} : ${answer_str} = ${true_answer_str}`);
+}
+
+const directMachine = new VigenereCipheringMachine();
+const reverseMachine = new VigenereCipheringMachine(false);
+
+assert_equal(directMachine.encrypt('attack at dawn!', 'alphonse'), 'AEIHQX SX DLLU!');
+assert_equal(directMachine.encrypt('Example of sequence: 1, 2, 3, 4.', 'lilkey'), 'PFLWTJP WQ CIOFMYMI: 1, 2, 3, 4.');
+assert_equal(directMachine.encrypt('cryptography', 'verylongkeyword'), 'XVPNECTXKTFU');
+assert_equal(directMachine.encrypt('Samelengthkey', 'Samelengthkey'), 'KAYIWIAMMOUIW');
+assert_equal(directMachine.encrypt('Same length key', 'Samelengthkey'), 'KAYI WIAMMO UIW');
+assert_equal(directMachine.decrypt('UWJJW XAGWLNFM VNNNDXHVWWL :)', 'js'), 'LEARN FRONTEND DEVELOPMENT :)');
+assert_equal(directMachine.decrypt('ICWWQAM KECEIK JVZZT EADGG!', 'rollingscopes'), 'ROLLING SCOPES SHOOL RULES!');
+assert_equal(directMachine.decrypt('TRVVFB VT JSUIFMYL!', 'learning'), 'INVEST IN YOURSELF!');
+assert_equal(directMachine.decrypt('HSVD AJAL ^^', 'behappy'), 'GOOD LUCK ^^');
+assert_equal(reverseMachine.encrypt('attack at dawn!', 'alphonse'), '!ULLD XS XQHIEA');
+assert_equal(reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse'), '!NWAD TA KCATTA');
--- src/what-season.js
+++ src/what-season.js
@@ -11,11 +11,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  
+  if (typeof date === 'undefined') return "Unable to determine the time of year!";
+  if (Object.prototype.toString.call(date) !== "[object Date]") throw new Error("Invalid date!");
+  if (Object.getOwnPropertyNames(date).length > 0) throw new Error("Invalid date!");
+  
+  const seasons = [
+    'winter',
+    'spring',
+    'summer',
+    'autumn',
+  ];
+
+  let m = date.getMonth() + 2;
+  if (m > 12) m = 1;
+  const s = Math.ceil(m / 3) - 1;
+  return `${seasons[s]}`;
 }
 
 module.exports = {
   getSeason
 };
+
+return;
+
+function assert_equal(answer, true_answer) {
+  answer_str = JSON.stringify(answer);
+  true_answer_str = JSON.stringify(true_answer);
+  reason = `${answer_str === true_answer_str}  `.slice(0, 6);
+  console.log(`is ${reason} : ${answer_str} = ${true_answer_str}`);
+}
+
+assert_equal(getSeason(new Date(2150, 7, 21, 18, 36, 41, 841)), 'summer');
+assert_equal(getSeason(new Date(83, 9, 25, 16, 20, 23, 544)), 'autumn');
+assert_equal(getSeason(new Date(81, 10, 13, 12, 50, 13, 493)), 'autumn');
+assert_equal(getSeason(new Date(481, 1, 5, 21, 58, 37, 272)), 'winter');
+assert_equal(getSeason(new Date(369, 9, 26, 4, 38, 0, 377)), 'autumn');
+assert_equal(getSeason(new Date(866, 2, 10, 12, 53, 10, 825)), 'spring');
+assert_equal(getSeason(new Date(2455, 5, 24, 5, 17, 19, 809)), 'summer');
+assert_equal(getSeason(new Date(1726, 7, 22, 23, 9, 55, 515)), 'summer');
+assert_equal(getSeason(new Date(655, 0, 1, 4, 56, 39, 676)), 'winter');
+assert_equal(getSeason(new Date(655, 11, 1, 4, 56, 39, 676)), 'winter');
+getSeason(new Date());
+getSeason();
