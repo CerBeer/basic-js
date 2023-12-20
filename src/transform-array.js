f1690045736e8c3691d0cf3144f7ea1724dd96c4
CerBeer <ocelopp@gmail.com>
2023-12-20 20:21:22 +0300

    feat: implements Transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,79 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
+  const result = [];
+  const status = [];
+//  arr.forEach((val) => status.push(typeof(val) === 'number' ? 1 : 0));
+  arr.forEach((val) => {
+    switch(val) {
+      case '--discard-next':
+        status.push(0);
+        break;
+      case '--discard-prev':
+        status.push(0);
+        break;
+      case '--double-next':
+        status.push(0);
+        break;
+      case '--double-prev':
+        status.push(0);
+        break;
+      default:
+        status.push(1);
+    }
+  });
+  arr.forEach((val, i) => {
+    switch(val) {
+      case '--discard-next':
+        if (i < arr.length - 1) {
+          status[i + 1] = status[i + 1] - 1;
+        }
+        break;
+
+      case '--discard-prev':
+        if (i > 0) {
+          status[i - 1] = status[i - 1] - 1;
+        }
+        break;
+    
+      case '--double-next':
+        if (i < arr.length - 1 && status[i + 1] > 0) {
+          status[i + 1] = status[i + 1] - 1 + 2;
+        }
+        break;
+
+      case '--double-prev':
+        if (i > 0 && status[i - 1] > 0) {
+          status[i - 1] = status[i - 1] - 1 + 2;
+        }
+        break;
+    }
+  });
+
+  status.forEach((val, idx) => {
+    if (val > 0) for (let i = 0; i < val; i += 1) result.push(arr[idx]);
+  });
+  
+  return result;
 }
 
 module.exports = {
   transform
 };
+
+return;
+
+function assert_equal(answer, true_answer) {
+  answer_str = JSON.stringify(answer);
+  true_answer_str = JSON.stringify(true_answer);
+  reason = `${answer_str === true_answer_str}  `.slice(0, 6);
+  console.log(`is ${reason} : ${answer_str} = ${true_answer_str}`);
+}
+
+assert_equal((transform([])), []);
+assert_equal(transform([1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5]), [1, 2, 3, 4, 5]);
+assert_equal(transform([1, 2, 3, '--double-next', 1337, '--double-prev', 4, 5]), [1, 2, 3, 1337, 1337, 1337, 4, 5]);
+assert_equal(transform([1, 2, 3, '--discard-next', 1337, '--discard-prev', 4, 5]), [1, 2, 3, 4, 5]);
+assert_equal(transform([1, 2, 3, '--double-next', 1337, '--discard-prev', 4, 5]), [1, 2, 3, 1337, 4, 5]);
