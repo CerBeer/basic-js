cb7c53b5883d8a94b8cb11a53049319893d38b53
CerBeer <ocelopp@gmail.com>
2023-12-20 15:40:38 +0300

    feat: implements Minesweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,11 +23,92 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  
+  const resultt = [];
+
+  const outerhl = [];
+  outerhl.push(9);
+  matrix[0].forEach((line) => outerhl.push(9));
+  outerhl.push(9);
+
+  resultt.push(outerhl);
+  matrix.forEach((line) => {
+    const newLine = [];
+    newLine.push(9);
+    line.forEach((val) => {
+      newLine.push(0);
+    })
+    newLine.push(9);
+    resultt.push(newLine);
+  });
+  resultt.push(outerhl);
+
+  matrix.forEach((line, li) => {
+    line.forEach((val, vi) => {
+      if (val) {
+        resultt[li + 0][vi + 0] = resultt[li + 0][vi + 0] + 1;
+        resultt[li + 0][vi + 1] = resultt[li + 0][vi + 1] + 1;
+        resultt[li + 0][vi + 2] = resultt[li + 0][vi + 2] + 1;
+        resultt[li + 1][vi + 0] = resultt[li + 1][vi + 0] + 1;
+        resultt[li + 1][vi + 1] = resultt[li + 1][vi + 1] + 1;
+        resultt[li + 1][vi + 2] = resultt[li + 1][vi + 2] + 1;
+        resultt[li + 2][vi + 0] = resultt[li + 2][vi + 0] + 1;
+        resultt[li + 2][vi + 1] = resultt[li + 2][vi + 1] + 1;
+        resultt[li + 2][vi + 2] = resultt[li + 2][vi + 2] + 1;
+      }
+    })
+  });
+
+  matrix.forEach((line, li) => {
+    line.forEach((val, vi) => {
+      if (val) {
+        resultt[li + 1][vi + 1] = 1;
+      }
+    })
+  });
+
+  const result = [];
+  for (let i = 1; i < resultt.length - 1; i += 1) {
+    result.push(resultt[i].slice(1, -1));
+  }
+
+  return result;
 }
 
 module.exports = {
   minesweeper
 };
+
+return;
+
+function assert_equal(answer, true_answer) {
+  answer_str = JSON.stringify(answer);
+  true_answer_str = JSON.stringify(true_answer);
+  reason = `${answer_str === true_answer_str}  `.slice(0, 6);
+  console.log(`is ${reason} : ${answer_str} = ${true_answer_str}`);
+}
+
+assert_equal(
+  minesweeper([
+    [true, false, false],
+    [false, true, false],
+    [false, false, false],
+  ]),
+  [
+    [1, 2, 1],
+    [2, 1, 1],
+    [1, 1, 1],
+  ],
+);
+
+assert_equal(
+  minesweeper([
+    [false, false, false],
+    [false, false, false],
+  ]),
+  [
+    [0, 0, 0],
+    [0, 0, 0],
+  ],
+);
