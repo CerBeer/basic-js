68e0d8c6b38a83694e56458e7ab1d870d5a5c4ab
CerBeer <ocelopp@gmail.com>
2023-12-20 18:58:53 +0300

    feat: implements Sum digits

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,11 +12,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  let result = n;
+  let rch = `${result}`;
+  while(rch.length > 1) {
+    result = rch.split('').reduce((rez, val) => rez + parseInt(val), 0)
+    rch = `${result}`;
+  }
+
+  return result;
 }
 
 module.exports = {
   getSumOfDigits
 };
+
+return;
+
+function assert_equal(answer, true_answer) {
+  answer_str = JSON.stringify(answer);
+  true_answer_str = JSON.stringify(true_answer);
+  reason = `${answer_str === true_answer_str}  `.slice(0, 6);
+  console.log(`is ${reason} : ${answer_str} = ${true_answer_str}`);
+}
+
+assert_equal(getSumOfDigits(91), 1);
+assert_equal(getSumOfDigits(100), 1);
+assert_equal(getSumOfDigits(35), 8);
+assert_equal(getSumOfDigits(99), 9);
+assert_equal(getSumOfDigits(123), 6);
